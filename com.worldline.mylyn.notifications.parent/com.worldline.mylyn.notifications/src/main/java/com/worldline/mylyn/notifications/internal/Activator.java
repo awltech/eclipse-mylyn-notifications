e9390fd4fc41c09dbf49b362fa9f9b79c5c61b3f
mvanbesien <mvaawl@gmail.com>
2013-09-24 11:35:01 +0200

    Initial Revision

---
+++ com.worldline.mylyn.notifications.parent/com.worldline.mylyn.notifications/src/main/java/com/worldline/mylyn/notifications/internal/Activator.java
@@ -0,0 +1,79 @@
+package com.worldline.mylyn.notifications.internal;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "com.worldline.mylyn.notifications"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		plugin.createImageRegistry();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin.getImageRegistry().dispose();
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Gets image, loaded from ImageRegistry
+	 * 
+	 * @param imageDescriptor
+	 * @return Image
+	 */
+	public static Image getImage(ImageDescriptor imageDescriptor) {
+		ImageRegistry imageRegistry = Activator.plugin.getImageRegistry();
+		String imageCode = String.valueOf(imageDescriptor.hashCode());
+		Image image = imageRegistry.get(imageCode);
+		if (image == null) {
+			image = imageDescriptor.createImage(true);
+			if (image != null)
+				imageRegistry.put(imageCode, image);
+		}
+		return image;
+	}
+
+}
